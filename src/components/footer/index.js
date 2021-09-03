f070564bec83713b1c24267c52e405dc7cca1fdd
Luis Igreja <lfsigreja@gmail.com>
2021-09-03 18:14:18 -0300

    footer done

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -9,11 +9,17 @@ export default class Footer {
   render() {
     if (this.elem) {
       this.elem.innerHTML = `
-        <div class=""> 
-          <label>Cadastre-se para receber nossas ofertas e novidades por e-mail</label>
-          <div>
-            <input placeholder="Digite o seu e-mail aqui" />
+        <div class="newsletter-section"> 
+          <label for="newsletter-input">Cadastre-se para receber nossas ofertas e novidades por e-mail</label>
+          <div class="newsletter-section__register">
+            <input id="newsletter-input" placeholder="Digite o seu e-mail aqui" />
             <button>Ok</button>
+          </div>
+          
+          <div class="newsletter-section__social">
+            <p>Acompanhe o Empório da Cerveja</p>
+            <a target="_blank"  href="https://www.facebook.com/EmporioDaCerveja"><i class="fab fa-facebook-square"></i></a>
+            <a target="_blank" href="https://www.instagram.com/empdacerveja/"><i class="fab fa-instagram"></i></a>
           </div>
         </div>
       `;
